9ae3b9490d977970d4c844e5fefb1bc7352de4af
lilaccode-PC\lilaccode <Changun824@gmail.com>
2016-11-20 23:20:10 +0900

    Main code add -User information -swich -while -title

--- main.c
+++ main.c
@@ -1,7 +1,73 @@
 #include<stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+#include<time.h>
+//====================
+
+//함수 넣는존
+
+//====================
 int main()
 {
-    printf("Fine,I'll going home:start");
-    printf("I'll going home project start!!!!");
-    return 0;
+	int main_switch_num = 0;  //메인문에 스위치 값을 받기 위한 변수 - 사용자 입력 값
+	int main_end=0;   //메인문을 종료시키기 위한 변수
+	system("title Learing Support Program");  //제목 변화
+	while (1)  //프로그램 반복
+	{  //while 중괄호
+
+		printf("=====================================================================\n");
+		printf("                    Learing Support Program \n");
+		printf("=====================================================================\n");
+		printf(" 1.학습 계획표\n");
+		printf(" 2.학습 통계표\n");
+		printf(" 3.계 산 기 \n");
+		printf(" 4.단 어 장\n");
+		printf(" 5.타 이 머\n");
+		printf(" 6.알 람\n");
+		printf(" 7.스톱워치\n");
+		printf(" 8.달 력\n");
+		printf(" 9.종 료\n");
+		printf("원하시는 메뉴에 숫자를 입력해주세요:");
+		scanf_s("%d", &main_switch_num);
+
+		switch (main_switch_num)
+		{
+		case 1:
+			//학습 계획표
+			break;
+		case 2:
+			//학습 통계표
+			break;
+		case 3:
+			//계산기 함수
+			break;
+		case 4:
+			//단어장
+			break;
+		case 5:
+			//타이머
+			break;
+		case 6:
+			//알람
+			break;
+		case 7:
+			//스톱워치
+			break;
+		case 8:
+			//달력
+		case 9:
+			main_end = 100;
+			break;
+		default:
+			printf("잘못 누르셨습니다.\n");
+			break;
+		}
+
+		if (main_end == 100)    //종료를 위해 쓰인 if문과 변수
+		{
+			printf("프로그램을 종료합니다.\n");
+			exit(1);  //탈출 명령어
+		}//if문 - 종료 중괄호
+	} //while 중괄호
+	return 0;
 }
